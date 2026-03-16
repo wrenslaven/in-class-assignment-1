330b4ff252c3ee72d3d8f3a5e1fc1fb59df785ac
wrens <wrenslaven@gmail.com>
2026-03-16 12:59:41 -0400

    made the quiz much more modular with a foreach loop and dictionary

--- trivia.cs
+++ trivia.cs
@@ -1,39 +1,37 @@
-Console.WriteLine("Welcome to the African capitals quiz!");
-Console.WriteLine("Question 1: What's the capital of Gabon?");
+using System.Collections.Generic;
+
+Dictionary<string, string> countryCapitals = new Dictionary<string, string>();
+countryCapitals.Add("Gabon", "Libreville");
+countryCapitals.Add("Burkina Faso", "Ouagadougou");
+countryCapitals.Add("Mozambique", "Maputo");
+countryCapitals.Add("Sudan", "Khartoum");
+
+List<string> countries = new List<string>()
+{
+    "Gabon",
+    "Burkina Faso",
+    "Mozambique"
+};
+
 int score = 0;
-string q1_input = Console.ReadLine();
-string q1_answer = "Libreville";
-if (q1_input == q1_answer) {
-    Console.WriteLine("That's correct!");
-    score++;
-}
-else
+int question_num = 1;
+
+Console.WriteLine("Welcome to the African capitals quiz! All answers are case-sensitive!");
+
+foreach (KeyValuePair<string, string> countryCapitalPair in countryCapitals)
 {
-    Console.WriteLine($"That's incorrect. The capital of Gabon is {q1_answer}.");
+    Console.WriteLine($"Question {question_num}: What's the capital of {countryCapitalPair.Key}?");
+
+    string q1_input = Console.ReadLine();
+    if (q1_input == countryCapitalPair.Value) {
+        Console.WriteLine("That's correct!");
+        score++;
+    }
+    else
+    {
+        Console.WriteLine($"That's incorrect. The capital of {countryCapitalPair.Key} is {countryCapitalPair.Value}.");
+    }
+    question_num++;
 }
 
-Console.WriteLine("Question 2: What's the capital of Burkina Faso?");
-string q2_input = Console.ReadLine();
-string q2_answer = "Ouagadougou";
-if (q2_input == q2_answer){
-    Console.WriteLine("That's correct!");
-    score++;
-}
-else
-{
-    Console.WriteLine($"That's incorrect. The capital of Gabon is {q2_answer}.");
-}
-
-Console.WriteLine("Question 3: What's the capital of Mozambique?");
-string q3_input = Console.ReadLine();
-string q3_answer = "Maputo";
-if (q3_input == q3_answer){
-    Console.WriteLine("That's correct!");
-    score++;
-}
-else
-{
-    Console.WriteLine($"That's incorrect. The capital of Gabon is {q3_answer}.");
-}
-
-Console.WriteLine($"That's the end of the quiz. Your final score is {score} out of 3.");
+Console.WriteLine($"That's the end of the quiz. Your final score is {score} out of {countryCapitals.Count}");
